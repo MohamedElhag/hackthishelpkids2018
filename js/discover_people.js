dcddaa829e3184997b5792e4684be5694ece96b8
Mohamed Elhag <melhag06@gmail.com>
2018-10-06 12:25:44 -0400

    Update js/discover_people.js

--- js/discover_people.js
+++ js/discover_people.js
@@ -3,44 +3,30 @@ var patients = [];
 var curr_name = "Mohamed";
 var curr_user = "swag";
 var curr_pass = "nvembu";
-var curr_nick = "iHaveAsthma";
 var curr_age = 10;
 var curr_ints = ["Soccer", "Dancing", "Fortnite"];
 var curr_pic = "none.jpg"
 
-function Demo_Person (f_name, l_name, user, pass, nick, age, ints, pic, med_id) {
+function Patient (f_name, l_name, user, pass, age, ints, pic, med_id) {
   this.name = f_name + " " + l_name;
   this.user = user;
   this.pass = pass;
-  this.nick = nick;
-  this.age = age;
-  this.ints = ints;
-  this.pic = pic;
-  this.med_id = med_id;
-  this.common = new array();
-}
-
-function Patient (f_name, l_name, user, pass, nick, age, ints, pic, med_id) {
-  this.name = f_name + " " + l_name;
-  this.user = user;
-  this.pass = pass;
-  this.nick = nick;
-  this.age = age;
-  this.ints = ints;
-  this.pic = pic;
+  this.age = 0;
+  this.ints = []];
+  this.pic = "";
   this.med_id = med_id;
 }
 
-var swag = new Patient(curr_name, curr_name, curr_user, curr_pass, curr_nick, curr_age, curr_ints, curr_pic, 1122332);
+var swag = new Patient(curr_name, curr_name, curr_user, curr_pass, curr_age, curr_ints, curr_pic, 1122332);
 
 
-var Navya = new Patient("Navya", "Kalale", "nva", "cscscs", "nav", 12, ["Soccer", "Art", "Fortnite", "fubol"], "hoes.jpg");
+var Navya = new Patient("Navya", "Kalale", "nva", "cscscs", 12, ["Soccer", "Art", "Fortnite", "fubol"], "hoes.jpg");
 
-var Monica = new Patient("Monica", "Chang", "mchang", "fuzzybear", "Mon", 10, ["History","Dancing","Fortnite"], "itME.jpg")
+var Monica = new Patient("Monica", "Chang", "mchang", "Mon", 10, ["History","Dancing","Fortnite"], "itME.jpg")
 
-var Nikhila = new Patient("Nikhila", "Vembu", "nvembu", "puppies", "Tequila", 12, ["Piano","Memes","Math"], "sos.jpg");
+var Nikhila = new Patient("Nikhila", "Vembu", "nvembu", "Tequila", 12, ["Piano","Memes","Math"], "sos.jpg");
 
-var Aldrich = new Patient("Aldrich", "Ronquillo", "alpal", "liveluvCS", "Buzz", 7, ["Piano","Math","History"], "yikes.jpg");
+var Aldrich = new Patient("Aldrich", "Ronquillo", "alpal", "Buzz", 7, ["Piano","Math","History"], "yikes.jpg");
 
 
 
